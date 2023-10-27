d52e3d77d97540cd17759e7c0d32c5fc80c45fe9
Darius Ramnath <39842396+Dariusramnath@users.noreply.github.com>
2023-10-27 11:32:34 -0400

    Changed header text

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -96,7 +96,6 @@ export const Header = () => {
           </div>
           <div className="flex flex-col">
             <span className="font-bold leading-tight">StegoSig</span>
-            <span className="text-xs">Ethereum dev stack</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
